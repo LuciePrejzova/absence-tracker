6f2c243113c158982de1c6d672bac950b6ffb95a
LuciePrejzova <89588897+LuciePrejzova@users.noreply.github.com>
2024-04-23 13:32:22 +0200

    Delete bin directory

--- bin/main/_data.sql
+++
@@ -1,5 +0,0 @@
--- Insert data into the kids table
-INSERT INTO kids (sys_id, first_name, last_name,  tel_number, email, user_name, password, substitutes, day_type)
-VALUES
-  (99, 'John', 'Doe', '123456789', 'john@example.com', 'JohnDoe', 'password', 0, 1),
-  (100, 'Alice', 'Smith', '987654321', 'alice@example.com', 'AliceSmith', 'password', 0, 1);
